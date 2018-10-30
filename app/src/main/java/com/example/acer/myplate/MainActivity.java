6daee618b2a2baae034c3411656db2eb6cd2ec39
SethMyers1 <sethnjmyers@hotmail.com>
2018-10-30 18:12:28 -0300

    basic implementation of the barcode api to work for one hard coded barcode image
    	new file:   .idea/assetWizardSettings.xml
    	modified:   .idea/caches/build_file_checksums.ser
    	modified:   app/build.gradle
    	modified:   app/src/main/java/com/example/acer/myplate/MainActivity.java
    	new file:   app/src/main/res/drawable-xxhdpi/puppy.png
    	modified:   app/src/main/res/layout/activity_main.xml

--- app/src/main/java/com/example/acer/myplate/MainActivity.java
+++ app/src/main/java/com/example/acer/myplate/MainActivity.java
@@ -2,6 +2,18 @@ package com.example.acer.myplate;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +21,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = (Button) findViewById(R.id.button);
+        final TextView txtView = (TextView) findViewById(R.id.txtContent);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImageView myImageView = (ImageView) findViewById(R.id.imgview);
+                //load image, decode it into a bitmap
+                Bitmap myBitmap = BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.puppy);
+                //set it as the bitmap for MyImageView
+                myImageView.setImageBitmap(myBitmap);
+                //set up barcode detector and make sure it is working
+                BarcodeDetector detector =
+                        new BarcodeDetector.Builder(getApplicationContext()).setBarcodeFormats(Barcode.DATA_MATRIX | Barcode.QR_CODE).build();
+                if(!detector.isOperational()){
+                    txtView.setText("Could not set up the detector!");
+                    return;
+                }
+                //create frame from bitmap, pass to detector
+                Frame frame = new Frame.Builder().setBitmap(myBitmap).build();
+                //return array of barcodes (if multiple barcodes are in the camera's frame)
+                SparseArray<Barcode> barcodes = detector.detect(frame);
+                //this will only process one barcode, and does not account for if there are no barcodes
+                //would have to be changed when we actually need it
+                Barcode thisCode = barcodes.valueAt(0);
+                txtView.setText(thisCode.rawValue);
+            }
+        });
+
     }
 }
