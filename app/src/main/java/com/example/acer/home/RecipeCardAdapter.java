9ab4d057e978f0b9b01bcc2a6148b30ea70d5c2f
Abinaya Raja <abinayaraja1411@gmail.com>
2018-12-01 00:30:10 -0400

    Recipe detail

--- app/src/main/java/com/example/acer/home/RecipeCardAdapter.java
+++ app/src/main/java/com/example/acer/home/RecipeCardAdapter.java
@@ -1,12 +1,16 @@
 package com.example.acer.home;
 
 import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -35,10 +39,25 @@ public class RecipeCardAdapter extends RecyclerView.Adapter<RecipeCardAdapter.My
 
     @Override
     public void onBindViewHolder(final MyViewHolder myViewHolder, int position) {
-        RecipeCard recipe = recipeList.get(position);
+        final RecipeCard recipe = recipeList.get(position);
         myViewHolder.txtRecipeName.setText(recipe.recipeName);
-        myViewHolder.recipeThumbnail.setImageResource(recipe.recipeThumbnail);
-    }
+        //http://square.github.io/picasso/
+        Picasso.with(mContext).load(recipe.recipeImageURL).into(myViewHolder.recipeImage);
+
+        //https://stackoverflow.com/questions/52096024/open-different-activities-by-clicking-on-different-cards-in-cardview-recyclervi
+        myViewHolder.recipeCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //https://stackoverflow.com/questions/50090135/pass-data-fragment-to-fragment-in-the-same-activity
+
+                Intent recipeDetail = new Intent(mContext, RecipeDetailActivity.class);
+                recipeDetail.putExtra("recipeID", recipe.recipeID);
+                mContext.startActivity(recipeDetail);
+
+            }
+        });    
+	}
 
     @Override
     public int getItemCount() {
@@ -48,13 +67,14 @@ public class RecipeCardAdapter extends RecyclerView.Adapter<RecipeCardAdapter.My
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
         public TextView txtRecipeName;
-        public ImageView recipeThumbnail;
+        public ImageView recipeImage;
+        public CardView recipeCardView;
 
         public MyViewHolder(View view) {
             super(view);
             txtRecipeName = view.findViewById(R.id.txtRecipeName);
-            recipeThumbnail = view.findViewById(R.id.imgRecipe);
-
+            recipeImage = view.findViewById(R.id.imgRecipe);
+            recipeCardView = view.findViewById(R.id.recipeCardView);
         }
     }
 }
---
+++ app/src/main/java/com/example/acer/home/RecipeDetailActivity.java
@@ -0,0 +1,99 @@
+package com.example.acer.home;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class RecipeDetailActivity extends AppCompatActivity {
+
+    TextView txtRecipeName;
+    ImageView imgRecipeImageURL;
+    TextView txtIngredients;
+    TextView txtPreparationTime;
+    TextView txtPreparation;
+    TextView txtSourceLink;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_detail);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //https://stackoverflow.com/questions/46954409/how-to-send-data-from-fragment-to-another-activity
+        int recipeID = getIntent().getIntExtra("recipeID", 0);
+        //txtRecipeName = findViewById(R.id.recipeName);
+        imgRecipeImageURL = findViewById(R.id.recipeImage);
+        txtIngredients = findViewById(R.id.ingredients);
+        txtPreparationTime = findViewById(R.id.preparationTime);
+        txtPreparation = findViewById(R.id.preparation);
+        txtSourceLink = findViewById(R.id.sourceLink);
+        GetRecipeDetail(recipeID);
+    }
+
+    private RecipeDetail GetRecipeDetail(int recipeID) {
+        String url = getResources().getString(R.string.azureApiUrl) + "RecipeDetail?recipeID=" + recipeID;
+        final RecipeDetail recipeDetail = new RecipeDetail();
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>()
+        {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    recipeDetail.RecipeName = response.getString("RecipeName");
+                    recipeDetail.Ingredients = response.getString("Ingredients");
+                    recipeDetail.PreparationTime = response.getString("PreparationTime");
+                    recipeDetail.PreparationMethod = response.getString("PreparationMethod");
+                    recipeDetail.RecipeImageURL = response.getString("RecipeImageURL");
+                    recipeDetail.SourceLink = response.getString("SourceLink");
+                    BindResponseToDetailView(recipeDetail);
+                } catch (JSONException e) {
+                    Toast.makeText(getApplicationContext(), R.string.errorRetrievingRecipes, Toast.LENGTH_SHORT);
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener()
+        {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        SingletonRequestQueue.getInstance(getApplicationContext()).addToRequestQueue(request);
+        return recipeDetail;
+    }
+
+    private void BindResponseToDetailView(RecipeDetail recipeDetail) {
+
+        //txtRecipeName.setText(recipeDetail.RecipeName);
+        Picasso.with(getApplicationContext()).load(recipeDetail.RecipeImageURL).fit().into(imgRecipeImageURL);
+        txtIngredients.setText(recipeDetail.Ingredients);
+        txtPreparationTime.setText(recipeDetail.PreparationTime);
+        txtPreparation.setText(recipeDetail.PreparationMethod);
+        txtSourceLink.setText(recipeDetail.SourceLink);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem)
+    {
+        finish();
+        return true;
+    }
+
+}
