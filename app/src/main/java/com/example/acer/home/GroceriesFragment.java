e626e1177cff4a4b8d96bef958ebb798edb0a510
Your Name <th845969@dal.ca>
2018-12-05 21:29:16 -0400

    Update GroceriesFragment.java
    Scheduled notification

--- app/src/main/java/com/example/acer/home/GroceriesFragment.java
+++ app/src/main/java/com/example/acer/home/GroceriesFragment.java
@@ -1,8 +1,14 @@
 package com.example.acer.home;
 
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.CoordinatorLayout;
@@ -10,10 +16,12 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,7 +31,10 @@ import android.widget.Toast;
 import com.example.acer.home.Model.DBRepository;
 import com.example.acer.home.Model.GroceryModel;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -44,6 +55,7 @@ public class GroceriesFragment extends Fragment implements RecyclerItemTouchHelp
     GroceryCardAdapter groceryCardAdapter;
     List<GroceryModel> listAddedGrocery;
     View groceryScreenView;
+    private PendingIntent pendingIntent;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -53,6 +65,12 @@ public class GroceriesFragment extends Fragment implements RecyclerItemTouchHelp
         groceryRecyclerView = (RecyclerView) GroceriesView.findViewById(R.id.recyclerVwGroceries);
         FloatingActionButton btnFloatAdd = GroceriesView.findViewById(R.id.btnFloatAdd);
         groceryRecyclerView.setPaddingRelative(2, 2, 2, 2);
+        //Notification code starts
+        Intent alarmIntent =new Intent(getActivity(),AlarmReceiver.class);
+        pendingIntent = PendingIntent.getBroadcast(getContext(),0,alarmIntent,0);
+        String groceryItems= fetchExpiredItem();
+        scheduleNotification(getNotification(groceryItems));
+        //Notification code ends
         //Initializing the DB object
         DBRepository groceryRepository =new DBRepository(getContext());
         listAddedGrocery = groceryRepository.ViewGroceries();
@@ -127,7 +145,6 @@ public class GroceriesFragment extends Fragment implements RecyclerItemTouchHelp
         {
             String name = listAddedGrocery.get(viewHolder.getAdapterPosition()).getGroceryName();
             int RowID = listAddedGrocery.get(viewHolder.getAdapterPosition()).getBaseID();
-            // backup of removed item for undo purpose
             final GroceryModel deletedItem = listAddedGrocery.get(viewHolder.getAdapterPosition());
             final int deletedIndex = viewHolder.getAdapterPosition();
             groceryCardAdapter.removeItem(viewHolder.getAdapterPosition());
@@ -137,5 +154,45 @@ public class GroceriesFragment extends Fragment implements RecyclerItemTouchHelp
             snackbar.setActionTextColor(Color.YELLOW);
             snackbar.show();
         }
+    }
+
+    private void scheduleNotification(Notification notification) {
+
+        Intent notificationIntent = new Intent(getActivity(), AlarmReceiver.class);
+        notificationIntent.putExtra(AlarmReceiver.NOTIFICATION_ID, 2);
+        notificationIntent.putExtra(AlarmReceiver.NOTIFICATION, notification);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(getContext(), 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        //Sarmad Code
+        int interval = 1000 * 60*20;
+        /* Set the alarm to start at 10:30 AM */
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        calendar.set(Calendar.HOUR_OF_DAY,8 );
+        calendar.set(Calendar.MINUTE, 16);
+        calendar.set(Calendar.AM_PM,Calendar.PM);
+        //Sarmad Code end
+        AlarmManager alarmManager = (AlarmManager) getActivity().getSystemService(Context.ALARM_SERVICE);
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
+                1000 * 60 * 20, pendingIntent);
+        //Working code
+        //alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, futureInMillis, pendingIntent);
+    }
+    private Notification getNotification(String content) {
+        Notification.Builder builder = new Notification.Builder(getContext());
+        builder.setContentTitle("Groceries are getting expired !");
+        builder.setContentText(content);
+        builder.setSmallIcon(R.drawable.mybuddylogo);
+        return builder.build();
+    }
+
+    private String fetchExpiredItem()
+    {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("d-MM-yyyy");
+        Date todayDate = Calendar.getInstance().getTime();
+        String reportDate = simpleDateFormat.format(todayDate);
+        DBRepository dbRepository = new DBRepository(getContext());
+        List <String> lstExpiringProducts = dbRepository.GetExpiringGroceries(reportDate);
+        String expiredItemsName =  TextUtils.join(",", lstExpiringProducts);
+        return expiredItemsName;
     }
 }
